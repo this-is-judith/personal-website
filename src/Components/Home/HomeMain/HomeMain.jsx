a064f673489cf8614d94ec114b9d3d6bfbdbef20
Judith Osei-Tete <jao92@cornell.edu>
2024-04-09 14:44:42 -0500

    remove react icons

--- src/Components/Home/HomeMain/HomeMain.jsx
+++ src/Components/Home/HomeMain/HomeMain.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>This is home main</div>;
+  return (
+    <>
+      <div>This is home main</div>
+    </>
+  );
 };
 
 export default Header;
