887e94702c03bbf5833b829967b6a054e28d591b
Judith Osei-Tete <jao92@cornell.edu>
2024-04-10 17:45:24 -0500

    add rectangles to homeProjects right

--- src/Components/Home/HomeProjects/HomeProjects.jsx
+++ src/Components/Home/HomeProjects/HomeProjects.jsx
@@ -1,12 +1,46 @@
 import React from "react";
+import LeftRectangles from "../HomeAbout/LeftRectangles";
 
 import "./homeProjects.css";
 
 function HomeProjects() {
   return (
     <div className="homeProjects-container">
-      <section className="projectsSection projectsLeft">This is left</section>
-      <section className="projectsSection projectsRight">This is right</section>
+      <section className="projectsSection projectsLeft">
+        <div className="projectsLeftTop">
+          <p>03.</p>
+          <p>My Projects</p>
+        </div>
+
+        <div className="projectsLeftMiddle">
+          I've worked on a couple of things
+        </div>
+
+        <div className="projectsLeftBottom">
+          <LeftRectangles />
+        </div>
+      </section>
+
+      <section className="projectsSection projectsRight">
+        <div className="projectsRightTop">Most Recent Projects</div>
+        <div className="projectsRightBottom">
+          <div className="projectsRightBottomTop">
+            <svg width="600" height="170">
+              <rect x="150" y="10" width="260" height="160" fill="grey" />
+            </svg>
+          </div>
+          <div className="projectsRightBottomMiddle">
+            <svg width="600" height="170">
+              <rect x="250" y="10" width="260" height="160" fill="grey" />
+            </svg>
+          </div>
+          <div className="projectsRightBottomBottom">
+            <svg width="600" height="170">
+              <rect x="150" y="10" width="260" height="160" fill="grey" />
+            </svg>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
