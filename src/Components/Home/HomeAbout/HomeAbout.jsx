93dff13d78b66f6ecd6fdf5f0cea039f7e8785aa
Judith Osei-Tete <jao92@cornell.edu>
2024-04-09 20:19:22 -0500

    fix width problem with PlayCircle

--- src/Components/Home/HomeAbout/HomeAbout.jsx
+++ src/Components/Home/HomeAbout/HomeAbout.jsx
@@ -4,9 +4,9 @@ import "./homeAbout.css";
 const HomeAbout = () => {
   return (
     <div class="homeAbout-container">
-      <section class="aboutSection aboutLeft">a</section>
+      <section class="aboutSection aboutLeft"></section>
 
-      <section class="aboutSection aboutRight">b</section>
+      <section class="aboutSection aboutRight"></section>
     </div>
   );
 };
