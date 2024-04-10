6dcad3c152a9cc373c80eb4ec43f00dc97bec10d
Judith Osei-Tete <jao92@cornell.edu>
2024-04-10 13:44:04 -0500

    add rectangles to homeAbout right

--- src/Components/Home/HomeAbout/HomeAbout.jsx
+++ src/Components/Home/HomeAbout/HomeAbout.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "./homeAbout.css";
+import Rectangle from "./Rectangle";
 
 const HomeAbout = () => {
   return (
-    <div class="homeAbout-container">
-      <section class="aboutSection aboutLeft"></section>
+    <div className="homeAbout-container">
+      <section className="aboutSection aboutLeft">
+        <div className="aboutLeftTop">
+          <p>01.</p>
+          <p>Who am I?</p>
+        </div>
 
-      <section class="aboutSection aboutRight"></section>
+        <div className="aboutLeftMiddle">
+          I'm a person of many interests and hobbies
+        </div>
+
+        <div className="aboutLeftBottom"></div>
+      </section>
+
+      <section className="aboutSection aboutRight">
+        <div className="aboutRightTop">
+          <Rectangle className="faithReactangle" />
+        </div>
+        <div className="aboutRightMiddle">
+          <Rectangle className="educationRectangle" />
+        </div>
+        <div className="aboutRightBottom">
+          <Rectangle className="hobbiesRectangle" />
+        </div>
+      </section>
     </div>
   );
 };
---
+++ src/Components/Home/HomeAbout/Rectangle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Rectangle() {
+  return (
+    <svg width="400" height="200">
+      <rect x="100" y="10" width="250" height="160" fill="grey" />
+    </svg>
+  );
+}
+
+export default Rectangle;
