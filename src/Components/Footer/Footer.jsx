b6962255339e7a7cbb123e7fd8f39715232fc6e4
Judith Osei-Tete <jao92@cornell.edu>
2024-04-12 01:41:16 -0500

    update footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="footerSection section1">Section 1</div>
 
-      <div className="footerSection section2">Section 2</div>
+      <div className="footerSection section2">
+        Judith Osei-Tete | Copyright 2024
+      </div>
 
       <div className="footerSection section3">Section 3</div>
     </footer>
