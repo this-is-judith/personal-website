cf424346fc8704e38e1ac3252a8664567a43c1ba
Judith Osei-Tete <jao92@cornell.edu>
2024-04-11 23:29:53 -0500

    add style to homeAbout content and change button

--- src/Components/Pages/Home/HomeAbout/HomeAbout.jsx
+++ src/Components/Pages/Home/HomeAbout/HomeAbout.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import RightRectangle from "./RightRectangle";
-import LeftRectangles from "./LeftRectangles";
-import ButtonContainer from "../HomeExperience/ButtonContainer";
+import { Link } from "react-router-dom";
 
 import "./homeAbout.css";
 
@@ -11,16 +10,30 @@ const HomeAbout = () => {
     <div className="homeAbout-container">
       <section className="aboutSection aboutLeft">
         <div className="aboutLeftTop">
-          <p>01.</p>
-          <p>Who am I?</p>
+          <div>
+            <h4 className="font__4 number">01.</h4>
+          </div>
+
+          <div>
+            <h1 className="font__0 whoAmI">Who</h1>
+            <h1 className="font__0 whoAmI">am I?</h1>
+          </div>
         </div>
 
         <div className="aboutLeftMiddle">
-          I'm a person of many interests and hobbies
+          <h3 className="font__3 whoAmIAnswer">
+            <p>A child of God, an engineering student, a friend.</p>
+            <p>I'm a person of many interests and hobbies.</p>
+          </h3>
         </div>
 
         <div className="aboutLeftBottom">
-          <ButtonContainer />
+          {/* <ButtonContainer /> */}
+          <Link to="/about">
+            <div className="button-container">
+              <text className="button-text">LEARN MORE</text>
+            </div>
+          </Link>
         </div>
       </section>
 
--- src/Components/Pages/Home/HomeExperience/ButtonContainer.jsx
+++ src/Components/Pages/Home/HomeExperience/ButtonContainer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./buttonContainer.css";
 
 const ButtonContainer = () => {
   const containerStyle = {
