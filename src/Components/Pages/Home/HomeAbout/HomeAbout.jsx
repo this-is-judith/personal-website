f51fce0cba8210de4141d16eef2dbce992e52bbf
Judith Osei-Tete <jao92@cornell.edu>
2024-04-16 22:22:02 -0500

    add graduation cap and book icons to homeAbout

--- src/Components/Pages/Home/HomeAbout/HomeAbout.jsx
+++ src/Components/Pages/Home/HomeAbout/HomeAbout.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import RightRectangle from "./RightRectangle";
 import Button from "./Button";
 import { FaCross } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa6";
+import { FaBookOpen } from "react-icons/fa6";
 
 import "./homeAbout.css";
 
@@ -16,13 +18,13 @@ const HomeAbout = () => {
           </div>
 
           <div>
-            <h1 className="font__0 whoAmI">Who</h1>
-            <h1 className="font__0 whoAmI">am I?</h1>
+            <h1 className="font__0 who-am-I">Who</h1>
+            <h1 className="font__0 who-am-I">am I?</h1>
           </div>
         </div>
 
         <div className="about-left-middle">
-          <h3 className="font__3 whoAmIAnswer">
+          <h3 className="font__3 who-am-I-answer">
             <p>A child of God, an engineering student, a friend.</p>
             <p>I'm a person of many interests and hobbies.</p>
           </h3>
@@ -40,15 +42,17 @@ const HomeAbout = () => {
       </section>
 
       <section className="about-section about-right">
-        <div className="aboutRightTop">
-          <RightRectangle className="faithReactangle" />
+        <div className="about-right-top">
+          <RightRectangle className="faith-rectangle" />
           <FaCross className="cross" size={"3rem"} />
         </div>
-        <div className="aboutRightMiddle">
-          <RightRectangle className="educationRectangle" />
+        <div className="about-right-middle">
+          <RightRectangle className="education-rectangle" />
+          <FaGraduationCap className="cap" size={"3rem"} />
         </div>
-        <div className="aboutRightBottom">
-          <RightRectangle className="hobbiesRectangle" />
+        <div className="about-right-bottom">
+          <RightRectangle className="hobbies-rectangle" />
+          <FaBookOpen className="book" size={"3rem"} />
         </div>
       </section>
     </div>
