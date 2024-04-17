67924cd330c9332b9eb1758a2f2fb6b0b1930de6
Judith Osei-Tete <jao92@cornell.edu>
2024-04-17 05:34:33 -0500

    add screen on/off animation to laptop

--- src/Components/Pages/Home/HomeBlog/Laptop.jsx
+++ src/Components/Pages/Home/HomeBlog/Laptop.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./laptop.css";
 
 const Laptop = () => {
+  const [isScreenOn, setScreenOn] = useState(false); // State to toggle screen on/off
   const generateKey = (keyClass = "key") => <div className={keyClass}></div>;
+
+  // Function to toggle screen on/off
+  const toggleScreen = () => {
+    setScreenOn(!isScreenOn);
+  };
 
   return (
     <div className="laptop">
-      <div className="screen"></div>
+      <div
+        className={`screen ${isScreenOn ? "on" : ""}`}
+        onClick={toggleScreen}
+      >
+        <div className="camera"></div>
+      </div>
+
       <div className="keyboard">
         <div className="keyboard-row">
           {generateKey()} {generateKey()} {generateKey()} {generateKey()}
           {generateKey()} {generateKey()} {generateKey()} {generateKey()}
-          {generateKey("key wide-key")} {/* Typically backspace */}
+          {generateKey("key wide-key")} {/* for ackspace */}
         </div>
+
         <div className="keyboard-row">
-          {generateKey("key wide-key")} {/* Typically tab */}
+          {generateKey("key wide-key")} {/* for tab */}
           {generateKey()} {generateKey()} {generateKey()} {generateKey()}
           {generateKey()} {generateKey()} {generateKey()} {generateKey()}
-          {generateKey("key wide-key")} {/* Typically enter */}
+          {generateKey("key wide-key")} {/* for enter */}
         </div>
+
         <div className="keyboard-row">
-          {generateKey("key wide-key")} {/* Typically caps lock */}
+          {generateKey("key wide-key")} {/* for caps lock */}
           {generateKey()} {generateKey()} {generateKey()} {generateKey()}
           {generateKey()} {generateKey()} {generateKey()}
-          {generateKey("key wide-key")} {/* Typically shift */}
+          {generateKey("key wide-key")} {/* for shift */}
         </div>
+
         <div className="keyboard-row">
-          {generateKey("key wide-key")} {/* Typically shift */}
+          {generateKey("key wide-key")} {/* for shift */}
           {generateKey()} {generateKey()} {generateKey()}
-          {generateKey("key extra-wide-key")} {/* Spacebar */}
+          {generateKey("key extra-wide-key")} {/* for spacebar */}
           {generateKey()} {generateKey()} {generateKey()}
-          {generateKey("key wide-key")} {/* Typically ctrl */}
+          {generateKey("key wide-key")} {/* for ctrl */}
         </div>
       </div>
     </div>
