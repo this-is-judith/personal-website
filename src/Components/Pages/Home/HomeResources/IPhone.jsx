54486c28ca1ab9ec430fc15467c3358fa4d0df56
Judith Osei-Tete <jao92@cornell.edu>
2024-04-26 17:54:14 -0500

    add volume and power buttons to iphone

--- src/Components/Pages/Home/HomeResources/IPhone.jsx
+++ src/Components/Pages/Home/HomeResources/IPhone.jsx
@@ -9,6 +9,10 @@ function IPhone() {
           <div className="iphone-speaker"></div>
           <div className="iphone-camera"></div>
         </div>
+
+        <div className="iphone-volume-up"></div>
+        <div className="iphone-volume-down"></div>
+        <div className="iphone-power-button"></div>
       </div>
     </div>
   );
