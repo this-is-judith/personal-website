a9ab13c75cf3bf1557bbc0942d1cb91a9e993c32
Judith Osei-Tete <jao92@cornell.edu>
2024-04-26 18:32:27 -0500

    add power on effect to iphone power button

--- src/Components/Pages/Home/HomeResources/IPhone.jsx
+++ src/Components/Pages/Home/HomeResources/IPhone.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./iphone.css";
 
 function IPhone() {
+  const [isPowerOn, setIsPowerOn] = useState(false);
+
+  const togglePower = () => {
+    setIsPowerOn(!isPowerOn);
+  };
+
   return (
     <div className="iphone">
-      <div className="iphone-screen">
+      <div className={`iphone-screen ${isPowerOn ? "on" : ""}`}>
         <div className="iphone-notch">
           <div className="iphone-speaker"></div>
           <div className="iphone-camera"></div>
         </div>
 
+        {isPowerOn && (
+          <div className="iphone-welcome-message">Welcome to iPhone</div>
+        )}
+
         <div className="iphone-volume-up"></div>
         <div className="iphone-volume-down"></div>
-        <div className="iphone-power-button"></div>
+        <div className="iphone-power-button" onClick={togglePower}></div>
         <div className="iphone-swipe-bar"></div>
       </div>
     </div>
