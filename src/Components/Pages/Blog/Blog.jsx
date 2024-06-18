0acb1ba98c979c8b916976cd891ddc8ac64619e4
Judith Osei-Tete <jao92@cornell.edu>
2024-06-18 12:38:25 -0400

    reformat Experience and Blog page placeholders

--- src/Components/Pages/Blog/Blog.jsx
+++ src/Components/Pages/Blog/Blog.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Header from "../../Header/Header";
+import "./blog.css";
+
 function Blog() {
-  return <h1>Blog Coming Soon</h1>;
+  {
+    document.body.style.margin = 0;
+  }
+
+  return (
+    <>
+      <div className="blog-page">
+        <div class="header">
+          <Header />
+        </div>
+
+        <main className="blog-container">
+          <div className="blog-description">
+            Judith's Blog Page is Coming Soon :)
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Blog;
--- src/Components/Pages/Experience/Experience.jsx
+++ src/Components/Pages/Experience/Experience.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Header from "../../Header/Header";
+import "./experience.css";
+
 function Experience() {
-  return <h1>Experience Coming Soon</h1>;
+  {
+    document.body.style.margin = 0;
+  }
+
+  return (
+    <>
+      <div className="experience-page">
+        <div class="header">
+          <Header />
+        </div>
+
+        <main className="experience-container">
+          <div className="experience-description">
+            Judith's Experience Page is Coming Soon :)
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Experience;
