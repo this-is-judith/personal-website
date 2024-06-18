a6258b8ae0352d4168da3654870c06e38a229b3a
Judith Osei-Tete <jao92@cornell.edu>
2024-06-18 12:14:57 -0400

    change header style and remove fixed z position

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,53 +11,51 @@ const Header = () => {
 
   return (
     <header className="header-container">
-      <nav className="header-bar">
-        <div className="header-section-1">
-          <Link to="/" onClick={handleScrollToTop}>
-            <img className="logo" src={logo} alt="logo"></img>
-          </Link>
-        </div>
+      <div className="header-section-1">
+        <Link to="/" onClick={handleScrollToTop}>
+          <img className="logo" src={logo} alt="logo"></img>
+        </Link>
+      </div>
 
-        <div className="header-section-2">
-          <ul className="header-content-list">
-            <li>
-              <Link to="/" className="header-link" onClick={handleScrollToTop}>
-                Home
-              </Link>
-            </li>
+      <div className="header-section-2">
+        <ul className="header-content-list">
+          <li>
+            <Link to="/" className="header-link" onClick={handleScrollToTop}>
+              Home
+            </Link>
+          </li>
 
-            <li>
-              <Link to="/about" className="header-link">
-                About
-              </Link>
-            </li>
+          <li>
+            <Link to="/about" className="header-link">
+              About
+            </Link>
+          </li>
 
-            <li>
-              <Link to="/experience" className="header-link">
-                Experience
-              </Link>
-            </li>
+          <li>
+            <Link to="/experience" className="header-link">
+              Experience
+            </Link>
+          </li>
 
-            <li>
-              <Link to="/projects" className="header-link">
-                Projects
-              </Link>
-            </li>
+          <li>
+            <Link to="/projects" className="header-link">
+              Projects
+            </Link>
+          </li>
 
-            <li>
-              <Link to="/blog" className="header-link">
-                Blog
-              </Link>
-            </li>
+          <li>
+            <Link to="/blog" className="header-link">
+              Blog
+            </Link>
+          </li>
 
-            <li>
-              <Link to="/resources" className="header-link">
-                Resources
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+          <li>
+            <Link to="/resources" className="header-link">
+              Resources
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
